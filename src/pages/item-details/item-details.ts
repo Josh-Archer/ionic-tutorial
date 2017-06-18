abe9b8222a2cb20111f908808f41804075054784
jarcher1200 <jarcher1200@gmail.com>
2017-06-17 23:34:50 -0400

    Added an alert and changed the primary color

--- src/pages/item-details/item-details.ts
+++ src/pages/item-details/item-details.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
 
+import { AlertController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-item-details',
@@ -10,8 +12,17 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ItemDetailsPage {
   selectedItem: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alerCtrl: AlertController) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
+  }
+
+  doAlert() {
+    let alert = this.alerCtrl.create({
+      title: 'Selected',
+      message: 'You sleected ' + this.selectedItem.title,
+      buttons: ['Ok']
+    });
+    alert.present();
   }
 }
